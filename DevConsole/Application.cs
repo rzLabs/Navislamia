88beee61d9accd0cf8027650efcd1ba80437a35c
Aodai <odai@odaiq.co>
2022-04-07 21:42:08 +0300

    DevConsole refactor

---
+++ DevConsole/Application.cs
@@ -0,0 +1,30 @@
+﻿using Configuration;
+using Navislamia.Game;
+using Notification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevConsole
+{
+    public class Application : IApplication
+    {
+        private IConfigurationService configurationService;
+        private INotificationService notificationService;
+        private IGameService gameService;
+
+        public Application(IConfigurationService configurationService, INotificationService notificationService, IGameService gameService)
+        {
+            this.configurationService = configurationService;
+            this.notificationService = notificationService;
+            this.gameService = gameService;
+        }
+
+        public void Run()
+        {
+            notificationService.WriteMarkup("It works!", Serilog.Events.LogEventLevel.Information);
+        }
+    }
+}
---
+++ DevConsole/IApplication.cs
@@ -0,0 +1,7 @@
+﻿namespace DevConsole
+{
+    public interface IApplication
+    {
+        void Run();
+    }
+}
