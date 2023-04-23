437d7dbc43d6e49dc57bbed21f97f327c6ab9fb8
Viktor Kuznecov <viktorkuznecov45@gmail.com>
2023-04-23 21:48:14 +0200

    fix config

--- DevConsole/Application.cs
+++ DevConsole/Application.cs
@@ -1,53 +1,64 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Configuration;
 using DevConsole.Exceptions;
 using DevConsole.Properties;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Navislamia.Game;
 
 namespace DevConsole
 {
-    public class Application
+    public class Application : IHostedService
     {
-        private readonly IConfiguration _configuration;
         private readonly ILogger<Application> _logger;
         private readonly IHostEnvironment _environment;
-        
-        private IGameService _gameService;
-        
-        public Application(IConfiguration configuration, IHostEnvironment environment, ILogger<Application> logger, IGameService gameService)
+        private readonly IGameService _gameService;
+        private readonly NetworkOptions _networkOptions;
+
+        public Application(IHostEnvironment environment, ILogger<Application> logger, IGameService gameService, IOptions<NetworkOptions> networkOptions)
         {
             _logger = logger;
-            _configuration = configuration;
             _environment = environment;
             _gameService = gameService;
+            _networkOptions = networkOptions.Value;
         }
         
-        public void Run()
+        public Task StartAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInformation("{Arcadia}\\n\\nNavislamia starting...\\n", Resources.arcadia);
+            _logger.LogInformation("{Arcadia}", Resources.arcadia);
+            _logger.LogInformation("Navislamia starting...");
             _logger.LogInformation("Environment: {EnvironmentName}", _environment.EnvironmentName);
 
-            var networkOptions = _configuration.GetSection("Network").Get<NetworkOptions>();
-            var ip = networkOptions.Ip;
-            var port = networkOptions.Port;
-            var backlog = networkOptions.Backlog;
+            var ip = _networkOptions.Ip;
+            var port = _networkOptions.Port;
+            var backlog = _networkOptions.Backlog;
             
             if (string.IsNullOrWhiteSpace(ip) || port == null)
             {
-                throw new InvalidConfigurationException("IP, Port or Backlog is either invalid or missing in configuration");
+                throw new InvalidConfigurationException("IP and/or Port or is either invalid or missing in configuration");
             }
 
             try
             {
-                _gameService.Start(ip, Convert.ToInt32(port), Convert.ToInt32(backlog));
+                _gameService.Start(ip, port, backlog);
             }
             catch (Exception e)
             {
+                StopAsync(cancellationToken);
                 throw new Exception($"Could not start Gameserver {e.InnerException}");
             }
+
+            Console.ReadLine();
+            return null;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return null;
         }
     }
 }
--- DevConsole/Program.cs
+++ DevConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Configuration;
 using Configuration.Options;
 using Database;
@@ -15,51 +17,50 @@ using Network;
 using Scripting;
 
 
-namespace DevConsole
+namespace DevConsole;
+
+
+class Program 
 {
-    internal static class Program
+    public static async Task Main(string[] args)
     {
-        public static Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            var app = ActivatorUtilities.CreateInstance<Application>(host.Services);
-            app.Run();
-            return Task.CompletedTask;
-        }
+        var host = CreateHostBuilder(args).Build();
+        await host.RunAsync();
+    }
 
-        private static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                    .ConfigureAppConfiguration((context, configuration) =>
-                    {
-                        configuration.Sources.Clear();
-                        configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                        configuration.AddEnvironmentVariables();
-                        configuration.AddCommandLine(args);
-                    })
-                    .ConfigureServices((context, services) =>
-                    {
-                        // Options
-                        services.AddOptions<DatabaseOptions>();
-                        services.AddOptions<NetworkOptions>();
-                        services.AddOptions<ScriptOptions>();
-                        services.AddOptions<MapOptions>();
-                        services.AddOptions<WorldOptions>();
-                        services.AddOptions<PlayerOptions>();
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, configuration) =>
+            {
+                configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                configuration.AddEnvironmentVariables("NAVISLAMIA_");
+            })
+            .ConfigureServices((context, services) =>
+            {
+                //Options
+                services.AddHostedService<Application>();
+                services.Configure<DatabaseOptions>(context.Configuration.GetSection("Database"));
+                services.Configure<NetworkOptions>(context.Configuration.GetSection("Network"));
+                services.Configure<ScriptOptions>(context.Configuration.GetSection("Script"));
+                services.Configure<MapOptions>(context.Configuration.GetSection("Map"));
+                services.Configure<WorldOptions>(context.Configuration.GetSection("World"));
+                services.Configure<PlayerOptions>(context.Configuration.GetSection("Player"));
 
-                        // Services
-                        services.AddSingleton<ICommandService, CommandModule>();
-                        services.AddSingleton<IDatabaseService, DatabaseModule>();
-                        services.AddSingleton<IWorldService, WorldModule>();
-                        services.AddSingleton<IScriptingService, ScriptModule>();
-                        services.AddSingleton<IMapService, MapModule>();
-                        services.AddSingleton<INetworkService, NetworkModule>();
-                        services.AddSingleton<IGameService, GameModule>();
-                    })
-                    .ConfigureLogging((context, logging) => {
-                        logging.AddConfiguration(context.Configuration.GetSection("Logging"));
-                        logging.AddConsole();
-                    });
-        }
+                // Services
+                services.AddTransient<ICommandService, CommandModule>();
+                services.AddTransient<IDatabaseService, DatabaseModule>();
+                services.AddTransient<IWorldService, WorldModule>();
+                services.AddTransient<IScriptingService, ScriptModule>();
+                services.AddTransient<IMapService, MapModule>();
+                services.AddTransient<INetworkService, NetworkModule>();
+                services.AddTransient<IGameService, GameModule>();
+            })
+            .ConfigureLogging((context, logging) => {
+                logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+            })
+            .UseConsoleLifetime();
     }
 }
+
